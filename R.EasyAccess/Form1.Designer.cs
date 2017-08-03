54648cac2556d4b90dd821ab5b5378fbc728eb6d
Roland Ecsegi <roland.ecsegi@outlook.com>
2017-08-03 22:14:25 +0300

    Initial commit

--- R.EasyAccess/Form1.Designer.cs
+++ R.EasyAccess/Form1.Designer.cs
@@ -28,12 +28,109 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.open = new System.Windows.Forms.Button();
+            this.list = new System.Windows.Forms.ComboBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.excel = new System.Windows.Forms.RadioButton();
+            this.word = new System.Windows.Forms.RadioButton();
+            this.path = new System.Windows.Forms.RadioButton();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // open
+            // 
+            this.open.Location = new System.Drawing.Point(212, 112);
+            this.open.Name = "open";
+            this.open.Size = new System.Drawing.Size(75, 23);
+            this.open.TabIndex = 0;
+            this.open.Text = "Open";
+            this.open.UseVisualStyleBackColor = true;
+            this.open.Click += new System.EventHandler(this.open_Click);
+            // 
+            // list
+            // 
+            this.list.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.list.FormattingEnabled = true;
+            this.list.Items.AddRange(new object[] {
+            "Item-1",
+            "Item-2",
+            "Item-3",
+            "Item-4",
+            "Item-5"});
+            this.list.Location = new System.Drawing.Point(212, 12);
+            this.list.Name = "list";
+            this.list.Size = new System.Drawing.Size(75, 21);
+            this.list.TabIndex = 1;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.path);
+            this.groupBox1.Controls.Add(this.word);
+            this.groupBox1.Controls.Add(this.excel);
+            this.groupBox1.Location = new System.Drawing.Point(12, 6);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(93, 129);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            // 
+            // excel
+            // 
+            this.excel.AutoSize = true;
+            this.excel.Location = new System.Drawing.Point(6, 10);
+            this.excel.Name = "excel";
+            this.excel.Size = new System.Drawing.Size(51, 17);
+            this.excel.TabIndex = 0;
+            this.excel.TabStop = true;
+            this.excel.Text = "Excel";
+            this.excel.UseVisualStyleBackColor = true;
+            // 
+            // word
+            // 
+            this.word.AutoSize = true;
+            this.word.Location = new System.Drawing.Point(6, 56);
+            this.word.Name = "word";
+            this.word.Size = new System.Drawing.Size(51, 17);
+            this.word.TabIndex = 1;
+            this.word.TabStop = true;
+            this.word.Text = "Word";
+            this.word.UseVisualStyleBackColor = true;
+            // 
+            // path
+            // 
+            this.path.AutoSize = true;
+            this.path.Location = new System.Drawing.Point(6, 105);
+            this.path.Name = "path";
+            this.path.Size = new System.Drawing.Size(47, 17);
+            this.path.TabIndex = 2;
+            this.path.TabStop = true;
+            this.path.Text = "Path";
+            this.path.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(299, 146);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.list);
+            this.Controls.Add(this.open);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "R>EasyAccess";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button open;
+        private System.Windows.Forms.ComboBox list;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton path;
+        private System.Windows.Forms.RadioButton word;
+        private System.Windows.Forms.RadioButton excel;
     }
 }
 
--- R.EasyAccess/Form1.cs
+++ R.EasyAccess/Form1.cs
@@ -1,12 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Configuration;
 using System.Windows.Forms;
+using Microsoft.Office.Interop.Excel;
+using Excel = Microsoft.Office.Interop.Excel;
+using Word = Microsoft.Office.Interop.Word;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
 
 namespace R.EasyAccess
 {
@@ -15,6 +14,172 @@ namespace R.EasyAccess
         public Form1()
         {
             InitializeComponent();
+        }
+
+        // Open Excel from configuration //
+
+        private void Proccess(string excel, string sheet)
+        {
+            string fileExcel = ConfigurationManager.AppSettings[excel];          
+            Excel.Application xlApp;
+            Excel.Workbook xldoc;
+            Excel.Worksheet xlsheet;
+            object misValue = System.Reflection.Missing.Value;
+            xlApp = new Excel.Application();
+            xldoc = xlApp.Workbooks.Open(fileExcel, 0, false, 5, "", "", false, Excel.XlPlatform.xlWindows, "", true, false, 0, true, false, false);
+            // open desired sheet from configuration //
+            try
+            {
+                xlsheet = (Excel.Worksheet)xldoc.Sheets[ConfigurationManager.AppSettings[sheet]];
+                xlsheet.Visible = Excel.XlSheetVisibility.xlSheetVisible;
+                xlsheet.Activate();
+                xlApp.Visible = true;
+                xlApp.UserControl = false;
+                xlApp.DisplayAlerts = false;
+            }
+            // bypass opening sheet //
+            catch (System.Runtime.InteropServices.COMException)
+            {
+                xlApp.Visible = true;
+                xlApp.UserControl = false;
+                xlApp.DisplayAlerts = false;
+            }
+        }
+
+        // Open Word from configuration  //
+        public void ProccessWord(string file)
+        {
+            string filepath = ConfigurationManager.AppSettings[file];
+            object misvl = System.Reflection.Missing.Value;
+            Word.Application wdApp = new Word.Application();
+            Word.Document doc = wdApp.Documents.Open(filepath, ReadOnly: false);
+            wdApp.Visible = true;
+            
+            /*
+            Process p = new Process();
+            p.StartInfo.FileName = file;
+            p.Start("WINWORD.EXE", );
+            */
+        }
+
+        // Open Path from configuration //
+        private void Path(string path)
+        {
+            System.Diagnostics.Process.Start(@"Explorer.exe", ConfigurationManager.AppSettings[path]);
+        }
+
+        private void open_Click(object sender, EventArgs e)
+        {
+            int mode;
+            if (excel.Checked)
+            {
+                mode = 1;
+            }
+            else if (word.Checked)
+            {
+                mode = 2;
+            }
+            else if (path.Checked)
+            {
+                mode = 3;
+            }
+            else
+            {
+                MessageBox.Show("select one of the tree options", @"Error", MessageBoxButtons.OK); // Insurance message, if you forgot to select an option!
+            }
+
+            ///// Opening Excel //////
+
+            if (excel.Checked)
+            {
+                if (list.Text == "")
+                {
+                    MessageBox.Show("You need to select an item from list!", @"Error", MessageBoxButtons.OK); // Insurance message, if you forgot to select an item from list!
+                    return;
+                }
+                switch (list.Text)
+                {
+                    case "Item-1":
+                        Proccess(@"Excel-Item-1", @"Sheet_Excel-Item-1");
+                        break;
+                    case "Item-2":
+                        Proccess(@"Excel-Item-2", @"Sheet_Excel-Item-2");
+                        break;
+                    case "Item-3":
+                        Proccess(@"Excel-Item-3", @"Sheet_Excel-Item-3");
+                        break;
+                    case "Item-4":
+                        Proccess(@"Excel-Item-4", @"Sheet_Excel-Item-4");
+                        break;
+                    case "Item-5":
+                        Proccess(@"Excel-Item-5", @"Sheet_Excel-Item-5");
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            ///// Opening Word //////
+
+            if (word.Checked)
+            {
+                if (list.Text == "")
+                {
+                    MessageBox.Show("You need to select an item from list!", @"Error", MessageBoxButtons.OK); // Insurance message, if you forgot to select an item from list!
+                    return;
+                }
+                switch (list.Text)
+                {
+                    case "Item-1":
+                        ProccessWord(@"Word-Item-1");
+                        break;
+                    case "Item-2":
+                        ProccessWord(@"Word-Item-2");
+                        break;
+                    case "Item-3":
+                        ProccessWord(@"Word-Item-3");
+                        break;
+                    case "Item-4":
+                        ProccessWord(@"Word-Item-4");
+                        break;
+                    case "Item-5":
+                        ProccessWord(@"Word-Item-5");
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            ///// Opening Path //////
+
+            if (path.Checked)
+            {
+                if (list.Text == "")
+                {
+                    MessageBox.Show("You need to select an item from list!", @"Error", MessageBoxButtons.OK); // Insurance message, if you forgot to select an item from list!
+                    return;
+                }
+                switch (list.Text)
+                {
+                    case "Item-1":
+                        Path(@"Path-Item-1");
+                        break;
+                    case "Item-2":
+                        Path(@"Path-Item-2");
+                        break;
+                    case "Item-3":
+                        Path(@"Path-Item-3");
+                        break;
+                    case "Item-4":
+                        Path(@"Path-Item-4");
+                        break;
+                    case "Item-5":
+                        Path(@"Path-Item-5");
+                        break;
+                    default:
+                        break;
+                }
+            }
         }
     }
 }
